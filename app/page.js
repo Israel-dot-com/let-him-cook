7e934788d11e12cf720dd8cd52340ebe7e2ea2b7
Israel <isltimi1000@gmail.com>
2023-08-07 19:15:27 +0100

    Homer change

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main>
-    <h1>Home</h1>
+    <h1>Homerrrr</h1>
     <Link href="/login"> Login </Link>
     <Link href="/signup"> Signup </Link>
     <Link href="/forgotpassword"> forgotpass </Link>
